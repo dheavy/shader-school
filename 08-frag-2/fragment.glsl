0385c77fb04098cfab72511bc4cb16c0a141d8d1
Davy Peter Braun <hello@davybraun.com>
2019-12-22 16:16:25 +0100

    Finish fragment shaders

--- 08-frag-2/fragment.glsl
+++ 08-frag-2/fragment.glsl
@@ -1,8 +1,16 @@
 precision mediump float;
 
-void main() {
+#define TILESIZE float(16.0)
 
-  //TODO: Replace this with a function that draws a checkerboard
-  
-  discard;
+bool inTile(vec2 p, float tileSize) {
+  vec2 ptile = step(0.5, fract(0.5 * p / tileSize));
+  return ptile.x == ptile.y;
+}
+
+void main() {
+  if (inTile(gl_FragCoord.xy, TILESIZE)) {
+    gl_FragColor = vec4(1.0);
+  } else {
+    discard;
+  }
 }
--- 09-frag-3/fragment.glsl
+++ 09-frag-3/fragment.glsl
@@ -5,8 +5,6 @@ uniform vec2 screenSize;
 
 void main() {
   vec2 coord = gl_FragCoord.xy / screenSize;
-
-  //TODO: Swap red and blue color channels of image
-
-  gl_FragColor = texture2D(texture, coord);
+  vec4 colors = texture2D(texture, coord);
+  gl_FragColor = vec4(colors.b, colors.g, colors.r, colors.a);
 }
