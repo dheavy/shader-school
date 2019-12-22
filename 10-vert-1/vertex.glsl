c8e0a518d87cc9075a04ad9a9d030ef372c72d2e
Davy Peter Braun <hello@davybraun.com>
2019-12-22 18:24:05 +0100

    Vertex basics

--- 10-vert-1/vertex.glsl
+++ 10-vert-1/vertex.glsl
@@ -4,9 +4,14 @@ uniform float theta;
 
 attribute vec2 position;
 
+vec2 rotate(vec2 uv, float rotation) {
+  float s = sin(rotation);
+  float c = cos(rotation);
+  mat2 m = mat2(c, s, -s, c);
+  return m * uv;
+}
+
 void main() {
-
   //TODO: rotate position by theta radians about the origin
-
-  gl_Position = vec4(position, 0, 1.0);
+  gl_Position = vec4(rotate(position, theta), 0, 1.0);
 }
